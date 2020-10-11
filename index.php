4b9e1364ff8115acdb9bed28fc138844c74fbae4
Zyovor <69038283+Zyovor@users.noreply.github.com>
2020-10-11 19:57:25 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,51 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+<body>
+<center>
+<form action="login.php" method="post">
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<table width="300" height="218" border="0" bordercolor="#000000">
+  <tr> 
+    <th colspan="3" bgcolor="#FFFFFF" scope="col">LOGIN</th>
+  </tr>
+  <tr>
+    <td width="227" bgcolor="#FFFFFF">Username</td>
+    <td width="36" bgcolor="#FFFFFF"><div align="center">:</div></td>
+    <td width="23" bgcolor="#FFFFFF"><label>
+      <input type="text" name="username" id="username"/>
+    </label></td>
+  </tr>
+  <tr>
+    <td bgcolor="#FFFFFF">Password</td>
+    <td bgcolor="#FFFFFF"><div align="center">:</div></td>
+    <td bgcolor="#FFFFFF"><label>
+      <input type="password" name="password" id="password" />
+    </label></td>
+  </tr>
+    <tr>
+  	<td width="227" bgcolor="#FFFFFF">Hak Akses</td>
+  	<td width="36" bgcolor="#FFFFFF"><div align="center">:</div></td>
+  	<td width="30" bgcolor="#FFFFFF"><label>
+     <label for="list">
+        <select name="tipe" id="tipe">
+          <option>0</option>
+          <option>1</option>
+        </select>
+  	</label></td>
+  <tr>
+    <td colspan="3" bgcolor="#FFFFFF"><div align="center">
+      <label>
+      <input type="submit" name="button" id="button" value="LOGIN" />
+      </label>
+    </div></td>
+  </tr>
+</table>
+</form>
+</center>
+</body>
+</html>
